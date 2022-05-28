6313fea41af68f98d794d4dc7cc87a21b8a83a07
Yacc5018 <sassou.est100@gmail.com>
2022-05-28 13:14:19 +0100

    init commit

---
+++ db.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const DB_URI = process.env.DB_URI;
+
+const dbConnect = function(){
+    mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        console.log('Connected to MongoDB');
+    }).catch(err => {
+        console.log('Error connecting to MongoDB: ', err.message);
+    });
+}
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    }
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+const exerciseSchma = new mongoose.Schema({
+    userId: {
+        type: String,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const exerciseModel = mongoose.model('Exercise', exerciseSchma);
+
+module.exports = {
+    dbConnect,
+    userModel,
+    exerciseModel
+}
--- index.js
+++ index.js
@@ -1,13 +1,94 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { dbConnect, userModel, exerciseModel } = require('./db.js');
 
-app.use(cors())
-app.use(express.static('public'))
+
+dbConnect();
+
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/api/users', (req, res) => {
+  const { username } = req.body;
+  userModel.find({ username }, (err, data) => {
+    if (err) {
+      res.json({error: err});
+    } else if (data.length > 0) {
+      res.json({error:'Username already exists'});
+    } else {
+      const newUser = new userModel({ username });
+      newUser.save((err, data) => {
+        if (err) {
+          res.json({error: err});
+        } else {
+          res.json(data);
+        }
+      });
+    }
+  });
+});
+
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const { _id, description, duration, date } = req.body;
+  const newExercise = new exerciseModel({ _id, description, duration, date });
+  newExercise.save((err, data) => {
+    if (err) {
+      res.json({error: err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+
+app.get('/api/users', (req, res) => {
+  userModel.find({}, (err, data) => {
+    if (err) {
+      res.json({error: err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+
+app.get('/api/users/:id/logs', (req, res) => {
+  const { _id, from, to, limit } = req.query;
+  const query = {};
+  if (_id) {
+    query._id = _id;
+  }
+  if (from) {
+    query.date = { $gte: new Date(from) };
+  }
+  if (to) {
+    query.date = { $lte: new Date(to) };
+  }
+  if (limit) {
+    query.limit = limit;
+  }
+  exerciseModel.find(query, (err, data) => {
+    if (err) {
+      res.json({error: err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+
 
 
 
@@ -15,4 +96,4 @@ app.get('/', (req, res) => {
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
